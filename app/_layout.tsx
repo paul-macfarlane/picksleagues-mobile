1047943014f071b256a45d04e45b83b72926ab6f
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-06-02 20:20:06 -0400

    sign up wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,7 +26,6 @@ export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
-
 export default function RootLayout() {
   const hasMounted = React.useRef(false);
   const { isDarkColorScheme } = useColorScheme();
